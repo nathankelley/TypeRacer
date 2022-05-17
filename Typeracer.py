0ecc534dd9e82b882145a282a4c403b4889a47c9
Cassie Smith <103961175+smittydumbell22@users.noreply.github.com>
2022-05-17 16:59:25 -0700

    Window

--- Typeracer.py
+++ Typeracer.py
@@ -3,14 +3,26 @@ import arcade
 
 
 ## Constants: This will be for the screen that the program opens a new window of
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 1200
 SCREEN_TITLE = "Typeracer"
 
 def draw_background():
     # This draws the background to fill the screen
 
-    arcade.draw_lrtb_rectangle_filled(0,SCREEN_WIDTH,SCREEN_HEIGHT,SCREEN_HEIGHT,arcade.color.WHITE_SMOKE)
+    # arcade.draw_lrtb_rectangle_filled(0,SCREEN_WIDTH,SCREEN_HEIGHT,SCREEN_HEIGHT,arcade.color.WHITE_SMOKE)
+        arcade.draw_lrtb_rectangle_filled(0,
+                                      SCREEN_WIDTH,
+                                      SCREEN_HEIGHT,
+                                      SCREEN_HEIGHT * (1 / 3),
+                                      arcade.color.SKY_BLUE)
+
+    # Draw the ground in the bottom third
+        arcade.draw_lrtb_rectangle_filled(0,
+                                      SCREEN_WIDTH,
+                                      SCREEN_HEIGHT / 3,
+                                      0,
+                                      arcade.color.DARK_SPRING_GREEN)
 
 def draw_actors():
     # When we create items to draw onto the background, lets put them into an array/list
