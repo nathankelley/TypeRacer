4f26a83440a1b6291c9edf1c8f8375c0326f97e4
Nate <kel18018@byui.edu>
2022-05-17 16:19:11 -0700

    RUN_PROGRAM loop
    While loop added that tracks whether game is running or over. Add code (functions, etc.).
    
    RUN_PROGRAM - starts out as true so loop starts with true;
    make false to end program

--- Typeracer.py
+++ Typeracer.py
@@ -1,50 +1,60 @@
 ## Library imports go Here
 import arcade
 
-
 ## Constants: This will be for the screen that the program opens a new window of
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Typeracer"
+RUN_PROGRAM = True
 
-def draw_background():
-    # This draws the background to fill the screen
+while RUN_PROGRAM:
+    
+    # get out of program when no longer playing
+    if RUN_PROGRAM == False:
+        print("Thanks for playing!")
+        break
+        
 
-    arcade.draw_lrtb_rectangle_filled(0,SCREEN_WIDTH,SCREEN_HEIGHT,SCREEN_HEIGHT,arcade.color.WHITE_SMOKE)
+    def draw_background():
+        # This draws the background to fill the screen
 
-def draw_actors():
-    # When we create items to draw onto the background, lets put them into an array/list
-    # and then we can draw the whole list here!
+        arcade.draw_lrtb_rectangle_filled(0,SCREEN_WIDTH,SCREEN_HEIGHT,SCREEN_HEIGHT,arcade.color.WHITE_SMOKE)
 
-    arcade.draw_actors()
+    def draw_actors():
+        # When we create items to draw onto the background, lets put them into an array/list
+        # and then we can draw the whole list here!
 
-# We'll start the render, call our draw functions, and finish the render :)
-# rendered. make sure to have set_window() active prior to these commands otherwise they will be useless
-arcade.start_render()
-draw_background()
-arcade.finish_render()
+        arcade.draw_actors()
 
-# Xander's Code for Key Input Retrieval
-# This should run outside of all other loops except for the main gameplay loop
-# The key 'Recieving input' is a simple check variable we can toggle between true and false for while the user is actively playing, as it will 
-#   decide if the keyboard inputs should be taken. I feel that we should have a point-and-click menu, something that benefits from this check.
+    # We'll start the render, call our draw functions, and finish the render :)
+    # rendered. make sure to have set_window() active prior to these commands otherwise they will be useless
+    arcade.start_render()
+    draw_background()
+    arcade.finish_render()
 
-# This is the user input string that will be checked against the block of text that the user is racing to type
-input_string = []
+    # Xander's Code for Key Input Retrieval
+    # This should run outside of all other loops except for the main gameplay loop
+    # The key 'Recieving input' is a simple check variable we can toggle between true and false for while the user is actively playing, as it will 
+    #   decide if the keyboard inputs should be taken. I feel that we should have a point-and-click menu, something that benefits from this check.
 
-# This is the function to recieve keypress input, more notes on how I plan to duplicate it for the rest of the table inside.
-def on_key_press(self, key, modifier):
+    # This is the user input string that will be checked against the block of text that the user is racing to type
+    input_string = []
 
-    if key == arcade.key.A:
+    # This is the function to recieve keypress input, more notes on how I plan to duplicate it for the rest of the table inside.
+    def on_key_press(self, key, modifier):
 
-        print("A has been pressed")
-        # A temporary test to show that on press works
-        # once we have the spacing finalized I'll create a table to recieve input for each key
+        if key == arcade.key.A:
 
-        input_string.append("a")
+            print("A has been pressed")
+            # A temporary test to show that on press works
+            # once we have the spacing finalized I'll create a table to recieve input for each key
+
+            input_string.append("a")
 
 
 
-# This line will keep the window up until someone closes it
-arcade.run
+    # This line will keep the window up until someone closes it
+    arcade.run
 
+else: 
+    RUN_PROGRAM = False
