caff9d9ee8caffc4c68ebf4fe96689d8068f9d3f
Nate <kel18018@byui.edu>
2022-06-06 21:16:48 -0700

    Added texts file with stories available for user

---
+++ texts/texts.py
@@ -0,0 +1,35 @@
+# This file holds files for the user to type.
+# They will be chosen at random for the user,
+# unless the user inputs their own text.
+
+# See https://americanliterature.com/short-stories-for-children
+
+# The Three Little Pigs
+threeLittlePigs = [
+    "Once upon a time there was an old mother pig who had three little pigs and not enough food to feed them. So when they were old enough, she sent them out into the world to seek their fortunes. The first little pig was very lazy. He didn't want to work at all and he built his house out of straw. The second little pig worked a little bit harder but he was somewhat lazy too and he built his house out of sticks. Then, they sang and danced and played together the rest of the day. The third little pig worked hard all day and built his house with bricks. It was a sturdy house complete with a fine fireplace and chimney. It looked like it could withstand the strongest winds. The next day, a wolf happened to pass by the lane where the three little pigs lived; and he saw the straw house, and he smelled the pig inside. He thought the pig would make a mighty fine meal and his mouth began to water."
+]
+
+# The Velveteen Rabbit
+velveteenRabbit = [
+    "There was once a velveteen rabbit, and in the beginning he was really splendid. He was fat and bunchy, as a rabbit should be; his coat was spotted brown and white, he had real thread whiskers, and his ears were lined with pink sateen. On Christmas morning, when he sat wedged in the top of the Boy’s stocking, with a sprig of holly between his paws, the effect was charming. There were other things in the stocking, nuts and oranges and a toy engine, and chocolate almonds and a clockwork mouse, but the Rabbit was quite the best of all. For at least two hours the Boy loved him, and then Aunts and Uncles came to dinner, and there was a great rustling of tissue paper and unwrapping of parcels, and in the excitement of looking at all the new presents the Velveteen Rabbit was forgotten."
+]
+
+# Jack and the Beanstalk
+jackBeanstalk = [
+    "Once upon a time there lived a poor widow and her son Jack. One day, Jack’s mother told him to sell their only cow. Jack went to the market and on the way he met a man who wanted to buy his cow. Jack asked, “What will you give me in return for my cow?” The man answered, “I will give you five magic beans!” Jack took the magic beans and gave the man the cow. But when he reached home, Jack’s mother was very angry. She said, “You fool! He took away your cow and gave you some beans!” She threw the beans out of the window. Jack was very sad and went to sleep without dinner. The next day, when Jack woke up in the morning and looked out of the window, he saw that a huge beanstalk had grown from his magic beans! He climbed up the beanstalk and reached a kingdom in the sky. There lived a giant and his wife. Jack went inside the house and found the giant’s wife in the kitchen. Jack said, “Could you please give me something to eat? I am so hungry!” The kind wife gave him bread and some milk."
+]
+
+# Little Red Riding Hood
+littleRedHood = [
+    "Once upon a time there was a dear little girl who was loved by every one who looked at her, but most of all by her grandmother, and there was nothing that she would not have given to the child. Once she gave her a little cap of red velvet, which suited her so well that she would never wear anything else. So she was always called Little Red Riding Hood. One day her mother said to her, 'Come, Little Red Riding Hood, here is a piece of cake and a bottle of wine. Take them to your grandmother, she is ill and weak, and they will do her good. Set out before it gets hot, and when you are going, walk nicely and quietly and do not run off the path, or you may fall and break the bottle, and then your grandmother will get nothing. And when you go into her room, don't forget to say, good-morning, and don't peep into every corner before you do it.'"
+]
+
+# Snow White
+snowWhite = [
+    "There was once a queen who had no children, and it grieved her sorely. One winter's afternoon she was sitting by the window sewing when she pricked her finger, and three drops of blood fell on the snow. Then she thought to herself: 'Ah, what would I give to have a daughter with skin as white as snow and cheeks as red as blood.' After a while a little daughter came to her with skin as white as snow and cheeks as red as blood. So they called her Snow White. But before Snow White had grown up, her mother, the Queen, died and her father married again, a most beautiful princess who was very vain of her beauty and jealous of all women who might be thought as beautiful as she was. And every morning she used to stand before her mirror and say: 'Mirror, mirror, on the wall, Who is the fairest of us all?'"
+]
+
+# The Reluctant Dragon
+reluctantDragon = [
+    "Footprints in the snow have been unfailing provokers of sentiment ever since snow was first a white wonder in this drab-coloured world of ours. In a poetry-book presented to one of us by an aunt, there was a poem by one Wordsworth, in which they stood out strongly—with a picture all to themselves, too—but we didn't think very highly either of the poem or the sentiment. Footprints in the sand, now, were quite another matter, and we grasped Crusoe's attitude of mind much more easily than Wordsworth's. Excitement and mystery, curiosity and suspense—these were the only sentiments that tracks, whether in sand or in snow, were able to arouse in us. We had awakened early that winter morning, puzzled at first by the added light that filled the room. Then, when the truth at last fully dawned on us and we knew that snow-balling was no longer a wistful dream, but a solid certainty waiting for us outside, it was a mere brute fight for the necessary clothes, and the lacing of boots seemed a clumsy invention, and the buttoning of coats an unduly tedious form of fastening, with all that snow going to waste at our very door."
+]
